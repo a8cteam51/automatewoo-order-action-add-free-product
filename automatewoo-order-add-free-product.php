87a366cac874c6abd2a72c78ea3de838b73ff3f5
NickGreen <nick.green@automattic.com>
2021-10-19 17:29:38 -0700

    Rename

--- automatewoo-order-add-free-product.php
+++ automatewoo-order-add-free-product.php
@@ -2,7 +2,7 @@
 
 /**
   * Plugin Name: AutomateWoo Order Action - Add Free Product
-	* Plugin URI:  https://github.com/a8cteam51/automatewoo-order-action-add-product
+	* Plugin URI:  https://github.com/a8cteam51/automatewoo-order-action-add-free-product
 	* Description: Extends the functionality of AutomateWoo with a custom action which allows you to add a product to an order as a line item.
 	* Version:     1.0.0
 	* Author:      WP Special Projects
