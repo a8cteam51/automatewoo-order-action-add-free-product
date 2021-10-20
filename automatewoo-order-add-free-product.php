8930f7b740debbff1fbcddf3e0c59cd81867a650
NickGreen <nick.green@automattic.com>
2021-10-20 09:59:59 -0700

    Separate main class file

--- automatewoo-order-add-free-product.php
+++ automatewoo-order-add-free-product.php
@@ -9,19 +9,8 @@
 	* Author URI:  https://wpspecialprojects.wordpress.com/
 	* License:     GPL v2 or later
   */
-namespace To51\AW_Action;
-
-class TO51_AW_Order_Add_Product {
-	public static function init() {
-		add_filter( 'automatewoo/actions', array( __CLASS__, 'register_action' ) );
+	if ( ! defined( 'ABSPATH' ) ) {
+		exit; // Exit if accessed directly
 	}
 
-	function register_action( $actions ) {
-		require_once __DIR__ . '/includes/class-action-order-add-free-product.php';
-
-		$actions['to51_add_free_product'] = Action_Order_Add_Free_Product::class;
-		return $actions;
-	}
-}
-
-TO51_AW_Order_Add_Product::init();
+	require_once dirname( __FILE__ ) . '/class-automatewoo-order-add-free-product.php';
